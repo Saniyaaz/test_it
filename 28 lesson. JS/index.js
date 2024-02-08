eb47153f5fbf17a178bebff0e1df9826e622d563
Сания М. Азмагамбетова <s.azmagambetova@dari.kz>
2024-02-08 12:19:12 +0600

    Все уроки до 29 числа

---
+++ 28 lesson. JS/index.js
@@ -0,0 +1,26 @@
+// function hello(name){
+//   console.log('Hello,' + name)
+// }
+
+// hello('Moldir')
+
+// function mappingNumber(item, index){
+//   console.log(item);
+// }
+// const mappingNumber2 = (item) => {
+//   console.log(item)
+// }
+// const mappingNumber3 = function (item) {
+//   console.log(item)
+// }
+
+// [1,2,3,4].map(mappingNumber)
+// [1,2,3,4].map(mappingNumber2)
+// [1,2,3,4].map(mappingNumber3)
+// [1,2,3,4].map((item)=>{
+//   console.log(item);
+// })
+// mappingNumber(1, 0)
+// mappingNumber(2, 1)
+// mappingNumber(3, 2)
+// mappingNumber(4, 3)
